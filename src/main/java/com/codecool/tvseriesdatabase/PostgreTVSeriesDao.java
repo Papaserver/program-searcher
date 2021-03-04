679d38959fca247901aab505660c9d4a77f2e066
Papaserver <miklossipko@gmail.com>
2021-03-04 19:43:11 +0100

    Connection ok with database, queries written in methods

--- src/main/java/com/codecool/tvseriesdatabase/PostgreTVSeriesDao.java
+++ src/main/java/com/codecool/tvseriesdatabase/PostgreTVSeriesDao.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class PostgreTVSeriesDao implements TVSeriesDao{
     public Connection getConnection(){
-        //TODO: rewrite this method to use your own db
+
         Connection connection = null;
-        String userName = "myuser";
-        String password = "mypass";
+        String userName = "papa";
+        String password = "password";
         String host = "localhost";
-        String databaseName = "test";
+        String databaseName = "tv_series";
 
 
         try {
@@ -23,28 +23,89 @@ public class PostgreTVSeriesDao implements TVSeriesDao{
     }
 
     public void add(Series series){
-        //TODO: implement this to add a new row to series table which contains data of series from the paramater
-    }
+        try (Connection con = getConnection()) {
+            String sql = "INSERT INTO series (title, num_of_seasons, genre) VALUES (?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, series.getTitle());
+            st.setInt(2, series.getNumberOfSeasons());
+            st.setString(3, series.getGenre());
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Connection failed!", e);
+        }
+     }
 
     public void update(Series series){
-        //TODO: implement this. It should update series in db which has the same title as the series in the parameter
-        // to have the same data as the series in parameter.
+        try (Connection con = getConnection()) {
+            String sql = "UPDATE series SET title = ?, num_of_seasons = ?, genre = ?";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, series.getTitle());
+            st.setInt(2, series.getNumberOfSeasons());
+            st.setString(3, series.getGenre());
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     public void delete(String titleToDelete) {
-        //TODO: implement this. It should delete series with title given in parameter.
+        try (Connection con = getConnection()) {
+            String sql = "DELETE FROM series WHERE title = ?";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, titleToDelete);
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     public List<Series> filterByGenre(String genre) {
-        //TODO: implement this. It should return list of series filtered by genre.
-        return null;
+        List<Series> filteredSeries = new ArrayList<>();
+        try (Connection con = getConnection()) {
+            String sql = "SELECT * FROM series WHERE genre = ?";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, genre);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Series series = new Series(rs.getString(1), rs.getInt(2), rs.getString(3));
+                filteredSeries.add(series);
+            }
+            return filteredSeries;
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     public void createTable() {
-        //TODO: implement this. It should create table named series. columns should be title as primary key, num_of_seasons, genre
+        try (Connection con = getConnection()) {
+            String sql = "CREATE TABLE series (\n" +
+                    "    title varchar PRIMARY KEY,\n" +
+                    "    num_of_seasons smallint,\n" +
+                    "    genre varchar" +
+                    ");";
+            Statement st = con.createStatement();
+            st.execute(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     public void dropTable() {
+        try (Connection con = getConnection()) {
+            String sql = "DROP TABLE IF EXISTS series;";
+            Statement st = con.createStatement();
+            st.execute(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
         //TODO: implement this. It should drop table called series if its exists on the db.
     }
 }
--- src/main/java/com/codecool/tvseriesdatabase/Series.java
+++ src/main/java/com/codecool/tvseriesdatabase/Series.java
@@ -1,10 +1,13 @@
 package com.codecool.tvseriesdatabase;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class Series {
     private String title;
     private int numberOfSeasons;
     private String genre;
 
-    //TODO: implement constructor and getters here
 }
